d58aad9d6173f05bd4260df3774afef88c6fde00
udaya07 <udayakumar.s@experionglobal.com>
2020-05-12 01:25:38 +0530

    routing add

--- src/app/modules/user-profile/user-profile.module.ts
+++ src/app/modules/user-profile/user-profile.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { UserProfileRoutingModule } from './user-profile-routing.module';
+
 @NgModule({
   declarations: [],
-  imports: [CommonModule]
+  imports: [CommonModule, UserProfileRoutingModule]
 })
 export class UserProfileModule {}
