5d6d9d514860a9bd6f210a7903cd2c950b3bb71f
udaya07 <udayakumar.s@experionglobal.com>
2020-05-12 14:35:50 +0530

    push into remote develop branch

--- src/app/modules/user-profile/user-profile-routing.module.ts
+++ src/app/modules/user-profile/user-profile-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TeamMembersComponent } from './team-members/team-members.component';
 
-const routes: Routes = [
-  {
-    path: 'team-members',
-    component: TeamMembersComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
