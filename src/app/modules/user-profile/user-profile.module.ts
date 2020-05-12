714ab68e85d8742ffbb4ff655e64fd8385c5f2ce
Malavika.S <malavika.s@experionglobal.com>
2020-05-12 15:42:40 +0530

    Updated dashboard.html

--- src/app/modules/user-profile/user-profile.module.ts
+++ src/app/modules/user-profile/user-profile.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserProfileRoutingModule } from './user-profile-routing.module';
-import { TeamMembersComponent } from './team-members/team-members.component';
 
 @NgModule({
-  declarations: [TeamMembersComponent],
+  declarations: [],
   imports: [CommonModule, UserProfileRoutingModule]
 })
 export class UserProfileModule {}
