62a14e30dcacd3d4c4f741f95041500e5c97d44a
udaya07 <udayakumar.s@experionglobal.com>
2020-05-13 20:40:25 +0530

    team-members-card component added

---
+++ src/app/modules/user-profile/team-members-card/team-members-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-members-card',
+  templateUrl: './team-members-card.component.html',
+  styleUrls: ['./team-members-card.component.scss']
+})
+export class TeamMembersCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/modules/user-profile/user-profile.module.ts
+++ src/app/modules/user-profile/user-profile.module.ts
@@ -4,9 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { UserProfileComponent } from './user-profile.component';
+import { TeamMembersCardComponent } from './team-members-card/team-members-card.component';
 
 @NgModule({
-  declarations: [EditProfileComponent, UserProfileComponent],
+  declarations: [
+    EditProfileComponent,
+    UserProfileComponent,
+    TeamMembersCardComponent
+  ],
   imports: [CommonModule, UserProfileRoutingModule]
 })
 export class UserProfileModule {}
