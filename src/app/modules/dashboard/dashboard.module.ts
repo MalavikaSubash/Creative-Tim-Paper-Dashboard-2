eb8b3ab6550d9ae8a9866a1175f7432b157cd194
preetika <preetika.prasad@experionglobal.com>
2020-05-14 10:08:17 +0530

    conflicts resolved

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
-
 import { DashboardComponent } from './dashboard.component';
 import { PiechartComponent } from './piechart/piechart.component';
 import { ChartsModule } from 'ng2-charts';
