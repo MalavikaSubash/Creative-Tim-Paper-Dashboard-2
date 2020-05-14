84f2b1b87942e1df3acb051f0bacab39bbb0fb7a
preetika <preetika.prasad@experionglobal.com>
2020-05-14 21:25:58 +0530

    created plainlist

---
+++ src/app/modules/table-list/plain-list/plain-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plain-list',
+  templateUrl: './plain-list.component.html',
+  styleUrls: ['./plain-list.component.scss']
+})
+export class PlainListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/modules/table-list/table-list.module.ts
+++ src/app/modules/table-list/table-list.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StripedListComponent } from './striped-list/striped-list.component';
 import { TableListComponent } from './table-list.component';
+import { PlainListComponent } from './plain-list/plain-list.component';
 
 @NgModule({
-  declarations: [StripedListComponent, TableListComponent],
+  declarations: [StripedListComponent, TableListComponent, PlainListComponent],
   imports: [CommonModule]
 })
 export class TableListModule {}
