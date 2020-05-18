2bf8f137fe93fc9f522cdfab6b1b300e610fe68e
Malavika.S <malavika.s@experionglobal.com>
2020-05-18 12:23:30 +0530

    Removed edit-profile

--- src/app/modules/user-profile/user-profile.module.ts
+++ src/app/modules/user-profile/user-profile.module.ts
@@ -2,7 +2,6 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserProfileRoutingModule } from './user-profile-routing.module';
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { UserProfileComponent } from './user-profile.component';
 import { TeamMembersCardComponent } from './team-members-card/team-members-card.component';
 import { ProfileDetailsComponent } from './profile-details/profile-details.component';
@@ -10,7 +9,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    EditProfileComponent,
     UserProfileComponent,
     TeamMembersCardComponent,
     ProfileDetailsComponent
