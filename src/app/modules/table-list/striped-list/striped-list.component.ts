e3d959e06a137e03b0c3620f486696e8f37d0fee
preetika <preetika.prasad@experionglobal.com>
2020-05-18 11:40:43 +0530

    completed stripedlist

--- src/app/modules/table-list/striped-list/striped-list.component.ts
+++ src/app/modules/table-list/striped-list/striped-list.component.ts
@@ -6,7 +6,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./striped-list.component.scss']
 })
 export class StripedListComponent implements OnInit {
+  simpleTable: any;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.simpleTable = [
+      {
+        Name: ' Dakota Rice',
+        City: 'Niger',
+        Country: 'Oud-Turnhout',
+        Salary: 36738
+      },
+
+      {
+        Name: 'Minerva Hooper',
+        City: 'Curaçao',
+        Country: 'Sinaai-Waas',
+        Salary: 23789
+      },
+      {
+        Name: ' Dakota Rice',
+        City: 'Niger',
+        Country: 'Oud-Turnhout',
+        Salary: 36738
+      },
+      {
+        Name: 'Minerva Hooper',
+        City: 'Curaçao',
+        Country: 'Sinaai-Waas',
+        Salary: 23789
+      },
+      {
+        Name: 'Minerva Hooper',
+        City: 'Curaçao',
+        Country: 'Sinaai-Waas',
+        Salary: 23789
+      },
+      {
+        Name: ' Dakota Rice',
+        City: 'Niger',
+        Country: 'Oud-Turnhout',
+        Salary: 36738
+      },
+      {
+        Name: 'Minerva Hooper',
+        City: 'Curaçao',
+        Country: 'Sinaai-Waas',
+        Salary: 23789
+      },
+      {
+        Name: ' Dakota Rice',
+        City: 'Niger',
+        Country: 'Oud-Turnhout',
+        Salary: 36738
+      }
+    ];
+  }
 }
